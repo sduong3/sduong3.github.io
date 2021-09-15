d7b16dc55c4402bec40979cde00081b72d244247
Steven Duong <stvenduong@gmail.com>
2021-09-15 16:13:01 -0700

    Sidedrawer is now more responsive. Changed homepage URL for github pages.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import "./App.css"
+import { makeStyles } from "@material-ui/core/styles"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import SideDrawer from "./components/layout/SideDrawer"
-import Home from "./components/pages/Home"
-import Resume from "./components/pages/Resume"
-import Projects from "./components/pages/Projects"
-import Contact from "./components/pages/Contact"
-import NotFound from "./components/pages/NotFound"
+import {
+  Home,
+  Resume,
+  Projects,
+  Contact,
+  NotFound
+} from "./components/pages/index.js"
 import { CssBaseline } from "@material-ui/core"
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex"
+  }
+}))
+
 function App() {
+  const classes = useStyles()
+
   return (
-    <div className="App">
+    <div className={classes.root}>
       <CssBaseline />
       <Router>
-        <div style={{ display: "flex" }}>
-          <SideDrawer />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/resume" component={Resume} />
-            <Route exact path="/projects" component={Projects} />
-            <Route exact path="/contact" component={Contact} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
+        <SideDrawer />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+          <Route component={NotFound} />
+        </Switch>
       </Router>
     </div>
   )
--- src/components/layout/SideDrawer.js
+++ src/components/layout/SideDrawer.js
@@ -1,5 +1,7 @@
 import { React, useState } from "react"
 import { Link } from "react-router-dom"
+import clsx from "clsx"
+import { makeStyles } from "@material-ui/core/styles"
 import {
   Drawer,
   List,
@@ -15,16 +17,48 @@ import HomeIcon from "@material-ui/icons/Home"
 import AssignmentIndIcon from "@material-ui/icons/AssignmentInd"
 import AppsIcon from "@material-ui/icons/Apps"
 import ContactIcon from "@material-ui/icons/ContactMail"
-import { makeStyles } from "@material-ui/core"
+
+const drawerWidth = 240
 
 const useStyles = makeStyles((theme) => ({
-  drawerPaper: { width: "inherit" },
-  drawerHeader: {
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+    border: "solid",
+    borderColor: "green"
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.enteringScreen
+    }),
+    [theme.breakpoints.down("sm")]: {
+      width: 150
+    }
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1
+    }
+  },
+  toolbar: {
     display: "flex",
     alignItems: "center",
+    justifyContent: "flex-end",
     padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
+    ...theme.mixins.toolbar
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
   },
   link: { textDecoration: "none", color: theme.palette.text.primary }
 }))
@@ -38,51 +72,48 @@ const navItems = [
 
 const SideDrawer = () => {
   const classes = useStyles()
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(false)
 
-  const handleDrawerOpen = () => {
-    setOpen(true)
-  }
-  const handleDrawerClose = () => {
-    setOpen(false)
+  const handleDrawerToggle = () => {
+    setOpen(!open)
   }
 
   return (
-    <div style={{ display: "flex" }}>
-      {!open ? (
-        <IconButton onClick={handleDrawerOpen}>
-          <ChevronRightIcon />
-        </IconButton>
-      ) : (
-        <Drawer
-          style={{ width: "240px" }}
-          variant="persistent"
-          anchor="left"
-          open={open}
-          classes={{ paper: classes.drawerPaper }}
-        >
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={handleDrawerClose}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </div>
-          <Divider />
-          <List>
-            {navItems.map((item, i) => (
-              <ListItem
-                button
-                key={i}
-                to={item.listPath}
-                className={classes.link}
-                component={Link}
-              >
-                <ListItemIcon>{item.listIcon}</ListItemIcon>
-                <ListItemText primary={item.listText} />
-              </ListItem>
-            ))}
-          </List>
-        </Drawer>
-      )}
+    <div className={classes.root}>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open
+          })
+        }}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerToggle}>
+            {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {navItems.map((item, i) => (
+            <ListItem
+              button
+              key={i}
+              to={item.listPath}
+              className={classes.link}
+              component={Link}
+            >
+              <ListItemIcon>{item.listIcon}</ListItemIcon>
+              <ListItemText primary={item.listText} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </div>
   )
 }
