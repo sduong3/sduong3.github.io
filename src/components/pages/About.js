6048e1d6992fd95905b525a7f918eea73ae5c57f
Steven Duong <stvenduong@gmail.com>
2021-09-15 22:56:10 -0700

    Added profile picture

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,10 +1,18 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import { Box, Grid, Typography } from "@material-ui/core"
+import profile from "../images/steven_profile_picture.jpg"
 import Typed from "react-typed"
+
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100vw"
+  },
+  imgContainer: {
+    objectFit: "cover",
+    width: "200px",
+    height: "250px",
+    borderRadius: "50%"
   },
   content: {
     height: "100vh",
@@ -26,6 +34,11 @@ const About = () => {
         alignItems="center"
         justifyContent="center"
       >
+        <img
+          src={profile}
+          className={classes.imgContainer}
+          alt="Profile picture of Steven"
+        />
         <Typography variant="h4">
           <Typed strings={["Steven Duong"]} typeSpeed={40} />
         </Typography>
